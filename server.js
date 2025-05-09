92ad8d0675bf6d0a1025300425affc32cca69d77
khallifah0006 <khallifah.825220006@stu.untar.ac.id>
2025-05-09 08:55:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.post('/api/recommendations', (req, res) => {
 
 // Serve index.html for all routes to support SPA routing
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start server
